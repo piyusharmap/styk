73468c56dad920d39f23d676080caa4692bb96cf
piyusharmap <piyusharmap@gmail.com>
2025-12-31 19:47:28 +0530

    implemented quit habit relapse

--- app/habit/[id].tsx
+++ app/habit/[id].tsx
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 import UIView from "../../components/ui/UIView";
 import UIText from "../../components/ui/UIText";
 import { Stack, useLocalSearchParams } from "expo-router";
+import { useHabitStore } from "../../store/habitStore";
 
 const HabitDetailsPage = () => {
 	const { id, color } = useLocalSearchParams<{
@@ -9,13 +10,15 @@ const HabitDetailsPage = () => {
 		color?: string;
 	}>();
 
+	const habitDetails = useHabitStore((s) => s.getHabitDetails(id));
+
 	return (
 		<>
 			<Stack.Screen
 				options={{ headerStyle: { backgroundColor: color } }}
 			/>
 			<UIView style={styles.container} isBottomSafe>
-				<UIText>{id}</UIText>
+				<UIText>{habitDetails?.name}</UIText>
 			</UIView>
 		</>
 	);
